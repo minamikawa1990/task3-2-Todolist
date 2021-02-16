4f2e38ccbc62ae2114908ab8d1daffcf45d34a0a
minamikawa1990 <ut.magnon@gmail.com>
2021-02-17 07:57:51 +0900

    削除ボタンの実装中

--- main.js
+++ main.js
@@ -2,148 +2,100 @@
 
 {
 
-    //追加ボタンを押下時にタスクを登録できる
+    // 追加ボタンを押下時にタスクを登録できる
 
     const add = document.getElementById('add');
     const input =document.getElementById('input');
     const tbodyTasks = document.getElementById('tasks');
-    const tbodyArray = [];
-    
+    const todos = [];
+    const btnRemove = document.createElement('button');
+    const tdRemove = document.createElement('td');
+    const trClass = document.getElementsByClassName('tr');
 
-    // tbodyにtr要素を追加
-    add.addEventListener('click' , () => {
-        // trをtbodyに追加
+    //タスクを表示する関数
+    function displayTodos() {
         
-        const trItem = document.createElement('tr');
-
-        tbodyArray.push(trItem);
-        tbodyTasks.appendChild(trItem);
-        
-        // IDとコメントをtdタグで作成
-        const td_ID = document.createElement('td');
-        const comment = document.createElement('td');
-        
-        for (let i = 0; i < tbodyArray.length; i++) {
-            td_ID.id = i;
+        const todo = {
+            task: input.value,
+            status: "作業中",
         }
+
+        // 配列に格納
+        todos.push(todo);
+
+        // tr要素をtbodyに追加
+        const trItem = document.createElement('tr');
+        trItem.classList.add('tr');
+        tbodyTasks.appendChild(trItem);
+
+
+        // IDの作成
+        const tdId = document.createElement('td');
+        for (let i = 0; i < todos.length; i++) {
+            tdId.id = i;
+        }
+        trItem.appendChild(tdId);
+        tdId.textContent = tdId.id;
+            
+         //コメントの追加
+         const comment = document.createElement('td');
+         comment.textContent = todo.task;
+         trItem.appendChild(comment);
+ 
+         //ボタンの追加（作業中）
+         const tdWorking = document.createElement('td');
+         const btnWorking = document.createElement('button');
+         trItem.appendChild(tdWorking);
+         tdWorking.appendChild(btnWorking);
+         btnWorking.textContent = todo.status;
+         console.log(tdWorking);
+         
+         //ボタンの追加（削除）
+         const tdRemove = document.createElement('td');
+         const btnRemove = document.createElement('button');
+         trItem.appendChild(tdRemove);
+         tdRemove.appendChild(btnRemove);
+         btnRemove.textContent = '削除';
+         btnRemove.id = tdId;
+         btnRemove.classList.add('remove');
+         console.log(tdRemove);
+ 
+         console.log(todos);
+         input.value = "";
+    }
+
+    // 削除ボタン押下時にその行を消す関数
+    function removeButton() {
+        const trClass = document.getElementsByClassName('tr');
+        for (let i = 0; i < trClass.length; i++) {
+            trClass[i].addEventListener('click', () => {
+                trClass[i].remove();
+            });
+        }
+    }
+    
+    // クリックイベント
+    add.addEventListener('click' , () => {
+        displayTodos();
+        console.log(trClass);
+
         
-        td_ID.textContent = td_ID.id;
-
-
-        comment.textContent = input.value;
-        const btn_working = document.createElement('button');
-        const btn_remove = document.createElement('button');
-        btn_working.textContent = "作業中";
-        btn_remove.textContent = "削除";
-
-        // 入力した列にコメント、ボタンなどの情報を付け加えている
-        trItem.appendChild(td_ID);
-        trItem.appendChild(comment);
-        trItem.appendChild(btn_working);
-        trItem.appendChild(btn_remove);
-        input.value = "";
-        
-        console.log(tbodyArray);
        
     })
 
-    
-
-   
-
-    
-    
+    removeButton();
 
     
 
-    // function btnChange () {
-    //     　const btn_working = document.createElement('button');
-    //       const workings = document.getElementsByClassName('working');
-    //     　btn_working.textContent = '作業中';
-    //     　workings[0].addEventListener('click', () => {
-    //     　　if (btn_working.textContent === '作業中') {
-    //     　　　btn_working.textContent = '完了';
-    //     　　} else {
-    //     　　　btn_working.textContent = '作業中';
-    //     　　}
-    //     　});
-    //     　return btn_working;
-    //     };
-    
-
-    // const radio_all = document.getElementById('radio_all');
-    // const radio_working = document.getElementById('radio_working');
-    // const radio_complete = document.getElementById('radio_complete');
-    // const add = document.getElementById('add');
-    // const ul = document.querySelector('ul');
-    // const input_task = document.getElementById('input');
-
-    // const li = document.createElement('li');
-    // const btn_working = document.createElement('button');
-    // const btn_remove = document.createElement('button');
-
-    
-    // // btn_working.addEventListener('click', () => {
-    // //     if(btn_working.textContent === '完了'){
-    // //         btn_working.textContent = '作業中';
-    // //     }else if(btn_working.textContent === '作業中'){
-    // //        btn_working.textContent = "完了";
-    // //    }
-    // // })
-    
-
-
-    // // radioボタンの仕分け関数
-    // function complete() {
-    //     radio_complete.addEventListener('click', () => {
-            
-    //     })
-    //     if(li.classList.contains('complete') === true) {
-
-    //     }
-    // }
-    // function working() {
-    //     if(li.classList.contains('working') === true) {
-
-    //     }
-    // }
-
-    // function all() {
-    //     if(li.classList.contains('all') === true) {
-
-    //     }
-    // }
-    
-   
-    // add.addEventListener('click', () => {
-
-    //     const li = document.createElement('li');
-    //     const btn_working = document.createElement('button');
-    //     const btn_remove = document.createElement('button');
-    
-    //     li.textContent = input_task.value;
-    //     btn_working.textContent = "作業中";
-    //     btn_remove.textContent = "削除"
-    //     btn_working.classList.add("working");
-    //     ul.appendChild(li);
-    //     li.appendChild(btn_working);
-    //     li.appendChild(btn_remove);
-    //     console.log(btn_working);
-    //     console.log(btn_remove);
-        
-    //     input_task.value = "";
-    // })
-
-    // const workings = document.getElementsByClassName('working');
-
-    
-    // btnChange();
     
     
 
 
+    
 
+    
 
-
+    
+    
 
 }
