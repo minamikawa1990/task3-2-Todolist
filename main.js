b9ae3c74e8033001d04f4b325824c025cb6b3a3f
minamikawa1990 <ut.magnon@gmail.com>
2021-03-29 09:15:27 +0900

    変数名、関数名を修正

--- main.js
+++ main.js
@@ -3,35 +3,35 @@
 {
     const add = document.getElementById('add');
     const input = document.getElementById('input');
-    const tbodyTasks = document.getElementById('tasks');
+    const tasks = document.getElementById('tasks');
     const todos = [];
 
-    const createBtnWorking = (status, row) => {
-        const btnWorking = document.createElement('button');
-        btnWorking.textContent = '作業中';
-        status.appendChild(btnWorking);
-        // btnWorking.addEventListener('click', () => {
+    const funcStatus = (status, row) => {
+        const createBtnStatus = document.createElement('button');
+        createBtnStatus.textContent = '作業中';
+        status.appendChild(createBtnStatus);
+        // createBtnStatus.addEventListener('click', () => {
         // }
-        return btnWorking;
+        return createBtnStatus;
     };
 
-    const createBtnRemove = (remove, row) => {
-        const btnRemove = document.createElement('button');
-        btnRemove.textContent = "削除";
-        remove.appendChild(btnRemove);
-        btnRemove.addEventListener('click', () => {
+    const funcRemove = (remove, row) => {
+        const createBtnRemove = document.createElement('button');
+        createBtnRemove.textContent = "削除";
+        remove.appendChild(createBtnRemove);
+        createBtnRemove.addEventListener('click', () => {
             const index = row.rowIndex - 1;
             todos.splice(index, 1);
             displayTodos();
         });
-        return btnRemove;
+        return createBtnRemove;
     };
 
     function displayTodos() {
-        tbodyTasks.innerText = "";
+        tasks.innerText = "";
         todos.forEach(todo => {
-            const todoId = tbodyTasks.rows.length;//tbody内の行の個数をId番号に指定
-            const row = tbodyTasks.insertRow(-1);//変数rowをtbodyTasksの最終行に追加
+            const todoId = tasks.rows.length;//tbody内の行の個数をId番号に指定
+            const row = tasks.insertRow(-1);//変数rowをtasksの最終行に追加
             row.classList.add('tasks');//rowにtasksクラスをつける
             const id = row.insertCell(0);//変数idをrowの1番目に挿入
             const comment = row.insertCell(1);//変数commentをrowの2番目に挿入
@@ -40,8 +40,8 @@
             id.innerText = todoId;//idのテキストをtodoIdに指定
             comment.innerText = todo.task;//コメントのテキストをオブジェクトの値で指定
             // status.innerText = todo.status;//コメントのテキストをオブジェクトの値で指定
-            createBtnRemove(remove, row);
-            createBtnWorking(status, row);
+            funcRemove(remove, row);
+            funcStatus(status, row);
             });
     }
 
